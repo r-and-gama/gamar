9a6d0de861b24794e0703f8f2eccaaccfc087ffe
Lucie Contamin <contamin.lucie@gmail.com>
2019-07-31 14:53:30 +0700

    Change order error message

--- R/run_experiment.R
+++ R/run_experiment.R
@@ -118,11 +118,11 @@ create_outdir <- function(dir) {
 #' @export
 run_experiment <- function(exp, hpc = 1, save = FALSE, path = NULL,
                            display = FALSE, append = TRUE) {
+  if (!is.experiment(exp))
+    stop("The argument \"exp\" is not an object of class \"experiment\".")
 
   if (ncol(obs_rates(exp)) == 0)
     return(exp)
-  if (!is.experiment(exp))
-    stop("The argument \"exp\" is not an object of class \"experiment\".")
 
   if (isTRUE(display) & !isTRUE(save)) {
     save <- TRUE
