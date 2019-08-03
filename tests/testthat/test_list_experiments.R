530e6fcc188632ec5480dc4221aaf1cbafad1e9e
meta00 <hobichhai@gmail.com>
2019-08-04 05:40:57 +0700

    more explicit name

--- tests/testthat/test_list_experiments.R
+++ tests/testthat/test_list_experiments.R
@@ -6,7 +6,7 @@ test_that("Test if no experiment is indicated in gaml file", {
   expect_null(list_experiments("test_files/sir_no_exp.gaml"))
 })
 
-test_that("Test gaml without type", {
+test_that("Test gaml experiment without type", {
   test1 <- list_experiments("test_files/sir_no_type.gaml")
   expect_equal(test1$type, "gui")
 })
