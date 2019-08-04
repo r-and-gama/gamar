5130881784df0d440f6d10f3b4e2c72cb7d108ac
meta00 <hobichhai@gmail.com>
2019-08-04 11:53:39 +0700

    add tests

--- tests/testthat/test_load_experiment.R
+++ tests/testthat/test_load_experiment.R
@@ -1,9 +1,11 @@
-testthat::test_that(
+test_that(
   "Tests returns NULL from load_experiment", {
     sir1 <- load_experiment("sir",
                             system.file("models", "sir.gaml",
                                         package = "gamar"))
     sir1[, ] <- NULL
-    testthat::expect_equal(rama:::get_parameters(sir1), NULL)
-    testthat::expect_equal(rama:::get_variables(sir1), NULL)
+    testthat::expect_equal(gamar:::get_parameters(sir1), NULL)
+    testthat::expect_equal(gamar:::get_variables(sir1), NULL)
+    testthat::expect_is(gamar:::make_df_dic(gamar:::get_parameters(sir1)),
+                               "list")
 })
